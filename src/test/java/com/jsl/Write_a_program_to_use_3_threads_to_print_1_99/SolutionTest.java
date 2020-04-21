cad4cccab0a77fc37e76e4bba2b2550693e4c563
shaoliang-jia-dragon <shaoliang@projectdrgn.com>
2020-04-21 17:24:42 +0800

    Add a new problem: print counter by 3 threads.

---
+++ src/test/java/com/jsl/Write_a_program_to_use_3_threads_to_print_1_99/SolutionTest.java
@@ -0,0 +1,14 @@
+package com.jsl.Write_a_program_to_use_3_threads_to_print_1_99;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    //TODO: This test case got a bug.
+    @Test
+    void testPrintCount() {
+        new Solution().printCountByMultipleThreads();
+    }
+}
